08775493fd2166b80c8378334459e30e2455af44
Martin_Maximus <Martin_Maximus@student.mahoningctc.com>
2020-12-09 13:01:08 -0500

    first commit

---
+++ JS Output Challenge/script.js
@@ -0,0 +1,14 @@
+// Enter your code below to edit index.html
+
+yourName = "max"
+document.getElementById("yourName").innerHTML = yourName;
+document.getElementById("myBio").innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo nemo optio dignissimos voluptatum tenetur recusandae fugiat, consequuntur mollitia dolor doloremque?';
+document.getElementById("collegeChoice").innerHTML = "Clown college";
+
+document.getElementById("programChoice").innerHTML = 'major in shananogans with a minor in tom foolery';
+document.getElementById("careerReason").innerHTML = 'clowns make 60k a year, and im out here doing it for free. it is about time i made a litle money off of what i do';
+document.getElementById("careerChoice").innerHTML = 'I want to be a clown';
+document.getElementById("companyChoice").innerHTML = ' i want to be self employed.';
+document.getElementById('answerOne').innerHTML = 'the Document Object Model says that a page is made up of objects'
+document.getElementById("answerTwo").innerHTML = 'it can rewrite html pages and send alerts.'
+document.getElementById("answerThree").innerHTML = 'any form of text or number'
